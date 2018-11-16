c54bcf3558a497803784e93e3ba560c892504d25
Huy Nguyen <huy.nguyen@act-on.net>
2018-11-15 21:19:38 -0800

    added replaced texts in files

---
+++ replace.ps1
@@ -0,0 +1,4 @@
+﻿$configFiles = Get-ChildItem . *.spec.js -rec
+foreach ($file in $configFiles) {
+    (Get-Content $file.PSPath) -replace "Setup: ", "" -join "`n" | Set-Content $file.PSPath -NoNewline
+}
